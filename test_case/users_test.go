d284534a485699e86d7654db2c557e6aecaaae24
Puneet Jindal <pjinda1@ext.uber.com>
2020-01-16 12:31:46 +0530

    updated db connection

--- test_case/users_test.go
+++ test_case/users_test.go
@@ -13,6 +13,7 @@ import (
 )
 
 func databaseCollection(collection string) error {
+	fmt.Println("In database function function")
 	mongoSession := config.ConnectDb(config.Database)
 	defer mongoSession.Close()
 
@@ -26,17 +27,19 @@ func databaseCollection(collection string) error {
 }
 
 func TestCreateEntry(t *testing.T) {
+	fmt.Println("enter the function")
 	gin.SetMode(gin.TestMode)
 
 	// Setup your router, just like you did in your main function, and
 	// register your routes.
 	var jsonStr = []byte(`{"name":"xyz","email":"xyz@pqr.com"}`)
 	r := gin.Default()
+	fmt.Println("database funciton")
 	databaseCollection("users")
 	r.POST("/api/user", controllers.SaveUserData)
 
 	req, err := http.NewRequest(http.MethodPost, "/api/user", bytes.NewBuffer(jsonStr))
-
+	fmt.Println("after req")
 	if err != nil {
 		t.Fatalf("Couldn't create request: %v\n", err)
 	}
